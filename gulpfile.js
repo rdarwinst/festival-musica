1498d1c71a3cec7530d4577d0fb4a2e287ce71ef
Darwin Ramirez <rdarwinst@gmail.com>
2024-07-29 16:25:02 -0500

    Actualizacion con GULP 5

--- gulpfile.js
+++ gulpfile.js
@@ -1,87 +1,32 @@
-'use strict';
-// CSS
-const { src, dest, watch, parallel } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('autoprefixer');
-const postcss = require('gulp-postcss');
-const cssnano = require('cssnano');
-const sm = require('gulp-sourcemaps');
-const plumber = require('gulp-plumber');
-// Imagenes
-const cache = require('gulp-cache');
-const imagemin = require('gulp-imagemin');
-const webp = require('gulp-webp');
-const avif = require('gulp-avif');
+import { src, dest, watch, series } from 'gulp';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
 
-// JavaScript
+const sass = gulpSass(dartSass);
 
-const terser = require('gulp-terser-js');
+// Rutas
+const paths = {
+    sass: 'src/scss/**/*.scss',
+    js: 'src/js/**/*.js',
+};
 
+// Compilar SASS
+export function css(done) {
+    src(paths.sass, {sourcemaps: true})
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('build/css', {sourcemaps: '.'}));
+    done();
+}
 
-function CSS(done) {
-    src('src/scss/**/*.scss')
-        .pipe(sm.init())
-        .pipe(plumber())
-        .pipe(sass())
-        .pipe(postcss([autoprefixer(), cssnano()]))
-        .pipe(sm.write('.'))
-        .pipe(dest('build/css'));
+export function js(done) {
+
+    src(paths.js)
+        .pipe( dest('build/js'));
 
     done();
 }
 
-function javascript(done) {
-    src('src/js/**/*.js')
-        .pipe(sm.init())
-        .pipe(terser())
-        .pipe(sm.write('.'))
-        .pipe(dest('build/js'));
-    done();
+export function dev() {
+    watch(paths.sass, css);
 }
-
-function imagenes(done) {
-    const opciones = {
-        optimizationLevel: 3
-    }
-    src('src/img/**/*{png,jpg}')
-        .pipe(cache(imagemin(opciones)))
-        .pipe(dest('build/img'));
-
-    done();
-}
-
-function convertirAvif(done) {
-    const opciones = {
-        quality: 50,
-        lossless: true
-    }
-    src('src/img/**/*.{png,jpg}')
-        .pipe(avif(opciones))
-        .pipe(dest('build/img'));
-    done();
-}
-
-function convertirWebp(done) {
-    const opciones = {
-        quality: 50
-    }
-    src('src/img/**/*{png,jpg}')
-        .pipe(webp(opciones))
-        .pipe(dest('build/img'));
-
-    done();
-}
-
-function dev(done) {
-    watch('src/scss/**/*.scss', CSS);
-    watch('src/js/**/*.js', javascript);
-    done();
-}
-
-exports.CSS = CSS;
-exports.js = javascript;
-exports.imagenes = imagenes;
-exports.convertirAvif = convertirAvif;
-exports.convertirWebp = convertirWebp;
-
-exports.dev = parallel(imagenes, convertirAvif, convertirWebp, javascript, dev);
+export default series(js, css, dev);
